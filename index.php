8eb03a5c28403bbe404d96c5896188ded03424ae
Bryce <80652391+xxBryce@users.noreply.github.com>
2022-03-26 15:10:12 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
       <?php require_once './assets/api/ri-api.php'; ?>
    </head>
    <body>
-      <div>
-         <img src="<?php echo $path . $img ?>"/>
-      </div>
+      <?php echo $path . $img ?>
    </body>
 </html>
