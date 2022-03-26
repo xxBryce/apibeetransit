fce5171b4e56db96558176bfb6f138f57582711f
Bryce <80652391+xxBryce@users.noreply.github.com>
2022-03-26 15:21:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <meta charset="utf-8" />
+    
       <title>random-image-api</title>
       <?php require_once './assets/api/ri-api.php'; ?>
    </head>
    <body>
-      <div>
+ 
          <a test<?php echo $path . $img ?>/>
-      </div>
+   
    </body>
 </html>
