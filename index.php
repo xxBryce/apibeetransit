c2c9fffd2f7f376d4922b8ad9ce7857160e8a197
Bryce <80652391+xxBryce@users.noreply.github.com>
2022-03-26 15:14:44 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8" />
-      <title>random-image-api</title>
       <?php require_once './assets/api/ri-api.php'; ?>
-   </head>
-   <body>
-     https://randombus.api.beetransit.ca <?php echo $path  $img ?>
-   </body>
+  <?php echo https://randombus.api.beetransit.ca$path  $img ?>
 </html>
